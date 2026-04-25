1aa26c61f9aa1220e90246db0f90f66f57b84227
flyingdragon1 <ungyu1996@gmail.com>
2026-04-25 14:29:14 +0900

    Initial commit: 쇼핑 리스트 앱

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const BASE = path.join(__dirname);
+
+http.createServer((req, res) => {
+  const name = req.url === '/' ? 'shopping-list.html' : req.url.replace(/^\//, '');
+  fs.readFile(path.join(BASE, name), (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(data);
+  });
+}).listen(8765, () => console.log('Server ready on 8765'));
